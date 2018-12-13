54217dedf502fb406b6330c17f2976772e3ad3fb
shin1x1 <shin1x1@gmail.com>
2018-12-13 16:22:28 +0900

    chown root /etc/postfix/sasl_passwd

--- postfix/goss/with-sasl_passwd/test_command.sh
+++ postfix/goss/with-sasl_passwd/test_command.sh
@@ -4,6 +4,7 @@ ID=$(docker run -d chatwork/postfix)
 IMAGE=chatwork/postfix-sasl_passwd
 
 docker cp /goss/sasl_passwd ${ID}:/etc/postfix/sasl_passwd
+docker exec ${ID} chown root:root /etc/postfix/sasl_passwd
 docker commit ${ID} ${IMAGE}
 
 /usr/local/bin/dgoss run ${IMAGE}

4b78981fac0a0e2d7b84fd1fde98c590d1218743
Ryo Abe <30817722+rabe1028@users.noreply.github.com>
2021-06-25 14:10:52 +0900

    SSS-632: fix aws cli installed path

--- gatling-s3-reporter/generate-report.sh
+++ gatling-s3-reporter/generate-report.sh
@@ -13,12 +13,12 @@ fi
 mkdir -p $S3_GATLING_RESULT_DIR_PATH
 
 # copy logs from s3
-/usr/bin/aws s3 cp s3://${S3_GATLING_BUCKET_NAME}/${S3_GATLING_RESULT_DIR_PATH}/ ${GATLING_HOME}/results/${S3_GATLING_RESULT_DIR_PATH} --recursive --exclude "*" --include "*.log"
+/usr/local/bin/aws s3 cp s3://${S3_GATLING_BUCKET_NAME}/${S3_GATLING_RESULT_DIR_PATH}/ ${GATLING_HOME}/results/${S3_GATLING_RESULT_DIR_PATH} --recursive --exclude "*" --include "*.log"
 
 # create report
 /opt/gatling/bin/gatling.sh -ro ${S3_GATLING_RESULT_DIR_PATH}
 
 # copy report files to s3 (excluding logs)
-/usr/bin/aws s3 cp ${GATLING_HOME}/results/${S3_GATLING_RESULT_DIR_PATH} s3://${S3_GATLING_BUCKET_NAME}/${S3_GATLING_RESULT_DIR_PATH}/ --recursive --exclude "*.log"
+/usr/local/bin/aws s3 cp ${GATLING_HOME}/results/${S3_GATLING_RESULT_DIR_PATH} s3://${S3_GATLING_BUCKET_NAME}/${S3_GATLING_RESULT_DIR_PATH}/ --recursive --exclude "*.log"
 
 echo [report url] https://${S3_GATLING_BUCKET_NAME}.s3.amazonaws.com/${S3_GATLING_RESULT_DIR_PATH}/index.html
